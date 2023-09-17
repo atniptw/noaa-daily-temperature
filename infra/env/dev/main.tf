58b73bdc51c7d9aceb5bcf93c049bbb3966607a1
Tom Atnip <atnip@sep.com>
2023-09-17 19:39:44 +0000

    Add terraform

---
+++ infra/env/dev/main.tf
@@ -0,0 +1,9 @@
+locals {
+  resource_group = "dev-rg-noaa"
+}
+
+module "infra" {
+  source = "../../"
+
+  resource_group = local.resource_group
+}
---
+++ infra/main.tf
@@ -0,0 +1,41 @@
+data "azurerm_resource_group" "rg" {
+  name = var.resource_group
+}
+
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_key_vault" "example" {
+  name                        = "examplekeyvault"
+  location                    = data.azurerm_resource_group.rg.location
+  resource_group_name         = data.azurerm_resource_group.rg.name
+  enabled_for_disk_encryption = true
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+
+  sku_name = "standard"
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    key_permissions = [
+      "Get",
+    ]
+
+    secret_permissions = [
+      "Backup",
+      "Delete",
+      "Get",
+      "List",
+      "Purge",
+      "Recover",
+      "Restore",
+      "Set"
+    ]
+
+    storage_permissions = [
+      "Get",
+    ]
+  }
+}
