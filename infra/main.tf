dc52d423fe88e4f4f41c8c01870e5e629222dce6
Tom Atnip <atnip@sep.com>
2023-09-24 16:59:51 -0400

    Enable system assigned identity

--- infra/main.tf
+++ infra/main.tf
@@ -64,6 +64,10 @@ resource "azurerm_data_factory" "factory" {
   name                = "devadfnoaa"
   location            = data.azurerm_resource_group.rg.location
   resource_group_name = data.azurerm_resource_group.rg.name
+
+  identity {
+    type = "SystemAssigned"
+  }
 }
 
 resource "azurerm_cosmosdb_account" "db" {
