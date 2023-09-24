cd0b28a014e852a687bec7599d0fa939187abd4e
Tom Atnip <atnip@sep.com>
2023-09-24 16:52:50 -0400

    Add db and data factory

--- infra/main.tf
+++ infra/main.tf
@@ -38,4 +38,46 @@ resource "azurerm_key_vault" "kv" {
       "Get",
     ]
   }
+
+  access_policy {
+    tenant_id = azurerm_data_factory.factory.identity[0].tenant_id
+    object_id = azurerm_data_factory.factory.identity[0].principal_id
+
+    key_permissions = [
+      "Get",
+    ]
+
+    secret_permissions = [
+      "Get",
+      "List",
+    ]
+
+    storage_permissions = [
+      "Get",
+    ]
+  }
+}
+
+resource "azurerm_data_factory" "factory" {
+  name                = "devadfnoaa"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+}
+
+resource "azurerm_cosmosdb_account" "db" {
+  name                = "devcosmosnoaa01"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+  offer_type          = "Standard"
+  kind                = "GlobalDocumentDB"
+
+  enable_automatic_failover = false
+  consistency_policy {
+    consistency_level = "Eventual"
+  }
+
+  geo_location {
+    location          = data.azurerm_resource_group.rg.location
+    failover_priority = 0
+  }
 }
