f21c23760ace444b499f030e10e338a394c89712
Tom Atnip <tom.atnip@gmail.com>
2023-10-01 19:55:54 +0000

    Add storage container

--- infra/main.tf
+++ infra/main.tf
@@ -105,3 +105,9 @@ resource "azurerm_role_assignment" "example" {
   role_definition_name = "Storage Blob Data Contributor"
   principal_id         = azurerm_data_factory.factory.identity[0].principal_id
 }
+
+resource "azurerm_storage_container" "example" {
+  name                  = "2023"
+  storage_account_name  = azurerm_storage_account.st.name
+  container_access_type = "private"
+}
