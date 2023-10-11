102f8ca1f79dd33a7ee0ccd14be87ba7d4dea928
Tom Atnip <tom.atnip@gmail.com>
2023-10-11 18:56:09 +0000

    Linked Service unique names

--- infra/modules/data_factory/main.tf
+++ infra/modules/data_factory/main.tf
@@ -9,13 +9,13 @@ resource "azurerm_data_factory" "factory" {
 }
 
 resource "azurerm_data_factory_linked_service_azure_blob_storage" "ghcn" {
-  name              = "ghcn"
+  name              = "ghcn_blob_storage"
   data_factory_id   = azurerm_data_factory.factory.id
   connection_string = var.storage_account_connection_string
 }
 
 resource "azurerm_data_factory_linked_service_cosmosdb" "grcn" {
-  name             = "ghcn"
+  name             = "ghcn_cosmosdb"
   data_factory_id  = azurerm_data_factory.factory.id
   account_endpoint = var.cosmosdb_endpoint
   account_key      = var.cosmosdb_primary_key
