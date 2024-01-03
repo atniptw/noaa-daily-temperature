62ded5b1cf667b39b531ce98e4446fdcd36a7c96
Tom Atnip <atnip@sep.com>
2024-01-03 16:20:43 -0500

    Add autoscale in tf

--- infra/modules/cosmos_db/main.tf
+++ infra/modules/cosmos_db/main.tf
@@ -11,8 +11,11 @@ resource "azurerm_cosmosdb_sql_container" "ghcn" {
   database_name         = azurerm_cosmosdb_sql_database.db.name
   partition_key_path    = "/Date"
   partition_key_version = 1
-  throughput            = 400
   default_ttl           = var.ttl
+
+  autoscale_settings {
+    max_throughput = 1000
+  }
 
   indexing_policy {
     indexing_mode = "consistent"
@@ -30,8 +33,11 @@ resource "azurerm_cosmosdb_sql_container" "stations" {
   database_name         = azurerm_cosmosdb_sql_database.db.name
   partition_key_path    = "/Date"
   partition_key_version = 1
-  throughput            = 400
   default_ttl           = var.ttl
+
+  autoscale_settings {
+    max_throughput = 1000
+  }
 
   indexing_policy {
     indexing_mode = "consistent"
