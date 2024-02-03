60444e65af076ce05a507391bffdb38cca5c8b2f
Tom Atnip <tom.atnip@gmail.com>
2024-02-03 18:45:19 +0000

    Remove stations container

--- infra/modules/cosmos_db/main.tf
+++ infra/modules/cosmos_db/main.tf
@@ -25,25 +25,3 @@ resource "azurerm_cosmosdb_sql_container" "ghcn" {
     }
   }
 }
-
-resource "azurerm_cosmosdb_sql_container" "stations" {
-  name                  = "stations"
-  resource_group_name   = var.resource_group_name
-  account_name          = var.cosmosdb_account_name
-  database_name         = azurerm_cosmosdb_sql_database.db.name
-  partition_key_path    = "/id"
-  partition_key_version = 1
-  default_ttl           = var.ttl
-
-  autoscale_settings {
-    max_throughput = 1000
-  }
-
-  indexing_policy {
-    indexing_mode = "consistent"
-
-    included_path {
-      path = "/*"
-    }
-  }
-}
